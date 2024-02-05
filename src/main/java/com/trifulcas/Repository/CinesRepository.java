98ad4b516aa299107abd127ec402c50b47710876
Yo231s <dawid.lempicki.1@gmail.com>
2024-02-05 19:47:49 +0100

    Cambios

--- src/main/java/com/trifulcas/Repository/CinesRepository.java
+++ src/main/java/com/trifulcas/Repository/CinesRepository.java
@@ -2,27 +2,14 @@ package com.trifulcas.Repository;
 
 import java.util.List;
 
-<<<<<<< HEAD
+
 import com.trifulcas.Models.Cines;
-=======
-import com.trifulcas.Controllers.PeliculasController;
-import com.trifulcas.Models.CinesModels;
->>>>>>> 25e526a8709a323ed687eb4b0ba91ad0bcd51da7
+
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface CinesRepository extends JpaRepository<Cines, Integer> {
     List<Cines> findByNameContaining(String name);
 
     List<Cines> findCategoryByFilmsFilmId(int id);
 
-    Iterable<Object> findBycinesFilmId(int id);
+    Iterable<Object> findBycinesFilmId(int id)
 
-<<<<<<< HEAD
-=======
-public interface CinesRepository extends JpaRepository<CinesModels, Integer> {
-
-    static void save(PeliculasController ciudades) {
-    }
-
-    List<CinesModels> findByCiudadContaining(String cines);
->>>>>>> 25e526a8709a323ed687eb4b0ba91ad0bcd51da7
-}
--- src/main/java/com/trifulcas/Repository/CiudadesRepository.java
+++ src/main/java/com/trifulcas/Repository/CiudadesRepository.java
@@ -7,15 +7,12 @@ import com.trifulcas.Models.Ciudades;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-<<<<<<< HEAD
 public interface CiudadesRepository extends JpaRepository<Ciudades, Long> {
     static Iterable<Object> findByCiudadesModelsId(int id) {
         return null;
     }
 
-    static List<Ciudades> findByNameContaining(String name) {
-        return null;
-    }
+
 
     static List<Ciudades> findByCiudadesFilmId(int id) {
         return null;
@@ -30,18 +27,5 @@ public interface CiudadesRepository extends JpaRepository<Ciudades, Long> {
     void deleteAllById(Long id);
 
     Optional<Object> findById(Class<? extends String> aClass);
-=======
-public interface CiudadesRepository extends JpaRepository<CiudadesModels, Integer> {
-    static Iterable<Object> findByCiudadesModelsId(int id) {
-        return null;
-    }
 
-    // Busco las categorías cuyo nombre contenga una cadena
-    static List<CiudadesModels> findByNameContaining(String name);
 
-    // Busco las categorías cuya propiedad 'Films' tengan un id 'FilmId'
-    static List<CiudadesModels> findByFilmsFilmId(int id) {
-        return null;
-    }
->>>>>>> 25e526a8709a323ed687eb4b0ba91ad0bcd51da7
-}
